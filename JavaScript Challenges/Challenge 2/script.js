9d33dc997a95a2dd4bd79f5458cc19260a7ae8c7
Zizette <zchamaa@gmail.com>
2023-08-17 18:52:55 +0300

    Challenge 2 - Done with step 1

---
+++ JavaScript Challenges/Challenge 2/script.js
@@ -0,0 +1,17 @@
+// Zizette challenge2 
+let age = 60;
+if (age >= 20) {
+    console.log("You can enter this room.");
+}
+else {
+    console.log("You can't enter this room.");
+}
+const expression1 = 5 > 3;
+const expression2 = 10 === '10';
+const expression3 = true && false;
+const expression4 = !expression3;
+console.log("Expression1:",expression1);
+console.log("Expression2:", expression2);
+console.log("Expression3:",expression3);
+console.log("Expression4:", expression4);
+// end step1
