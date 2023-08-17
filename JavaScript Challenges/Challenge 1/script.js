94593bcd453f950da493fe737254a482523acd13
Zizette <zchamaa@gmail.com>
2023-08-17 14:58:04 +0300

    Challenge 1 - Done with Step 3

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,3 +1,14 @@
 console.log('I am linked now!');
-var name="Zizette";
+var name = "Zizette";
 alert(name);
+var age = 23;
+
+var lastName = "Chamaa";
+
+DOB = "09/21/2000";
+console.log("My name is " + name + lastName + ". My date of birth is : " + DOB+ " and my Age is " + age);
+lastName="Rices";
+name="Lea";
+DOB = "12/12/1960"
+age = 63;
+console.log(" CouCou ! My name is " + name + lastName + ". My date of birth is : " + DOB+ " and my Age is " + age);
