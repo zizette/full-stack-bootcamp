45c5145b86fb6d336ee96e43a54e89b7439c020d
Zizette <zchamaa@gmail.com>
2023-08-17 19:16:48 +0300

    Challenge 2 - Done with step 3

--- JavaScript Challenges/Challenge 2/script.js
+++ JavaScript Challenges/Challenge 2/script.js
@@ -10,9 +10,9 @@ const expression1 = 5 > 3;
 const expression2 = 10 === '10';
 const expression3 = true && false;
 const expression4 = !expression3;
-console.log("Expression1:",expression1);
+console.log("Expression1:", expression1);
 console.log("Expression2:", expression2);
-console.log("Expression3:",expression3);
+console.log("Expression3:", expression3);
 console.log("Expression4:", expression4);
 // end step1
 //start step2
@@ -20,15 +20,25 @@ const age = 25;
 const hasDriverLicense = true;
 const hasCar = false;
 canDrive = false;
-if(age >= 18 && hasDriverLicense === true )
-{ canDrive=true;}
-canRentCar=false; 
-if (age >= 22 && hasDriverLicense === true)
-{
-    canRentCar=true; 
+if (age >= 18 && hasDriverLicense === true) { canDrive = true; }
+canRentCar = false;
+if (age >= 22 && hasDriverLicense === true) {
+    canRentCar = true;
 }
-needDriver=false;
-if( age < 18 &&  hasDriverLicense===false && hasCar === false)
-{ needDriver=true;}
+needDriver = false;
+if (age < 18 && hasDriverLicense === false && hasCar === false) { needDriver = true; }
 
 //start step3
+var role = "admin";
+
+if (role == "admin") {
+    console.log('You have read and write access for the data');
+}
+else if (role === "user") {
+    console.log('You have only read access for the data');
+}
+else if (role === "guest") {
+    console.log('You have no access for the data');
+}
+else { console.log('User role is not recoginized'); }
+//End step 3
