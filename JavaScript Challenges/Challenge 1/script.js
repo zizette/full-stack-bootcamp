24cc7a810f0fd1f205b810d1b811e9732a28a5c0
Zizette <zchamaa@gmail.com>
2023-08-17 15:50:59 +0300

    Challenge 1 - Done with Step 4

--- JavaScript Challenges/Challenge 1/script.js
+++ JavaScript Challenges/Challenge 1/script.js
@@ -1,10 +1,11 @@
-console.log('I am linked now!');
+console.log('I am linked now!'); // Step 1
+// Step 2
 var name = "Zizette";
 alert(name);
 var age = 23;
 
 var lastName = "Chamaa";
-
+// Step 3
 DOB = "09/21/2000";
 console.log("My name is " + name + lastName + ". My date of birth is : " + DOB+ " and my Age is " + age);
 lastName="Rices";
@@ -12,3 +13,21 @@ name="Lea";
 DOB = "12/12/1960"
 age = 63;
 console.log(" CouCou ! My name is " + name + lastName + ". My date of birth is : " + DOB+ " and my Age is " + age);
+// Step 4
+console.log("This is a text", typeof "This is a text", true , typeof true , false , typeof false , 1215 , typeof 1215,typeof "999", '{}' , typeof `{}` , `[]` , typeof `[]` , null , typeof null, undefined , typeof undefined);
+let MyName = "Zizo"; // string
+console.log(MyName, typeof MyName);
+let zlength = 16;        // number
+console.log(zlength, typeof zlength);
+let household=["TV", "AC", "Frige"] ; // array object
+console.log(household, typeof household);
+let yes=true;       // boolean
+console.log(yes, typeof yes);
+
+const  person ={FrstName: "Zizo", lastName: "Chamaa", nationality: "Lebanese", married: true};    // Object
+console.log(person, typeof person);
+let no_one= null;  //Null datatype
+console.log(no_one, typeof no_one);
+
+let no_def=undefined;   //Undefined
+console.log(no_def, typeof no_def);
