c23d2a1af201c2f999cede10801970692d387108
Zizette <zchamaa@gmail.com>
2023-08-22 16:37:49 +0300

    Challenge 4 - Done with step 1

--- JavaScript Challenges/Challenge 3/zscript.js
+++ JavaScript Challenges/Challenge 3/zscript.js
@@ -1,5 +1,5 @@
 // Start step 1
-document.write("<h1> Challenge 3 Step1 </h1>")
+// document.write("<h1> Challenge 3 Step1 </h1>")
 var num1 = 5, num2 = 7;
 var num3 = addNumbers(num1, num2);
 console.log(num1, num2, "Result is : ", num3);
@@ -33,8 +33,7 @@ var result1 = compute(num1, num2, operation);
 console.log("compute has the following arguments:", num1, num2, operation, "and the result is : ", result1);
 
 function compute(num1, num2, operation) {
-    var num3 = num1;
-    var num4 = num2;
+
     switch (operation) {
         case "+":
 
@@ -55,7 +54,7 @@ function compute(num1, num2, operation) {
         //  return mul;
         case "/":
             return num1 / num2;
-        //   divNumbers(num3, num4) ;
+        //   divNumbers(num1, num2) ;
 
         default:
             return console.log("No such operator ", operation);
@@ -65,3 +64,58 @@ function compute(num1, num2, operation) {
 // End of Nested Function which are depreciated in this new version of java sorry it doesn't work why do a nested when I go directly
 
 
+// ## Step 2: Arrow Functions and Scopes
+// document.write("<h1> Challenge 3  Start Step2 </h1>")
+let num11 = 48;
+
+function squareNumber(number) {
+    console.log(num11, "Global Variable inside the function");
+    console.log(number, "local variable");
+    return number * number;
+}
+console.log("The square of : the global variable ", num11, "is ", squareNumber(num11));
+console.log(" The local variable number is dead outside its mother function!!!!..");
+
+// ## Step 3: Loops
+for (let i = 1; i < 11; i++) { console.log("i is in for : ", i) }
+let i = 10;
+while (i > 0) {
+    i--;
+    { console.log("i is in while : ", i) }
+}
+
+let ii = 0;
+do {
+    ii++;
+    if (ii % 2 == 0) { console.log("ii is in do ... while and pair: ", ii) }
+}
+while (ii < 11);
+
+let fruits = ['apple', 'mango', 'banana', 'passionfruit', 'orangelemon', 'orange'];
+let fruit = "";
+for (fruit of fruits) {
+    if (fruit.length > 5) {
+        console.log('the number of characters is ', fruit.length, " and it is ", fruit);
+    }
+}
+// ### Advanced Loop Challenge
+fruits.forEach(fruit) = (fruit) => {
+    if (fruit.length > 5) { console.log(fruit) }
+
+};
+const numbers = [1, 2, 3, 4];
+function double(num) {
+    return num * 2;
+}
+function square(num) {
+    return num * num;
+}
+function processsArray(arrayNumbers, functionInput) {
+    let newArray = [];
+    for (let i = 0; i < arrayNumbers.length; i++) {
+        newArray[i] = functionInput(arrayNumbers[i])
+    }
+    return newArray;
+}
+console.log(processsArray(numbers, double));
+console.log(processsArray(numbers, square))
---
+++ JavaScript Challenges/Challenge 4/script.js
@@ -0,0 +1,14 @@
+// ## Goal of Challenge 4
+// ## Step 1: Arrays and Objects
+let fivenumbers = new Array;
+fivenumbers = ['5', '7', '10', '60'];
+console.log(fivenumbers);
+personalInfo = {
+    fname: "Zizette CH",
+    age: 60,
+    height: 162,
+    location: "Matn",
+    nationality: "Lebanese"
+};
+console.log(personalInfo);
+
