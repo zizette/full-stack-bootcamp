9eeeabe1200a2a5c0e4780159c7071e3a7712c0a
Zizette <zchamaa@gmail.com>
2023-08-22 17:22:37 +0300

    Challenge 4 - Done with step 2

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -1,7 +1,7 @@
 // ## Goal of Challenge 4
 // ## Step 1: Arrays and Objects
 let fivenumbers = new Array;
-fivenumbers = ['5', '7', '10', '60'];
+fivenumbers = [5, 7, 10, 60];
 console.log(fivenumbers);
 personalInfo = {
     fname: "Zizette CH",
@@ -11,4 +11,17 @@ personalInfo = {
     nationality: "Lebanese"
 };
 console.log(personalInfo);
+// ## Step 2: Spread Operator
+let nextThreeNumbers =new Array;
+nextThreeNumbers =[45, 66, 90];
+allNumbers={...fivenumbers, ...nextThreeNumbers};
+console.log("allNumbers is : ", allNumbers)
+
+let additionalInfo =new Object();
+additionalInfo ={
+    address: "Mar Elias street, BCHAR",
+    tel_Num: "+096170606987"
+}
+fullInfo={  ...personalInfo, ...additionalInfo};
+console.log("fullInfo is: ", fullInfo);
 
