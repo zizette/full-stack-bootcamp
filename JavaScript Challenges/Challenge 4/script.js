296829cdd9f44dee660bd3e1d04488ff35e111a3
Zizette <zchamaa@gmail.com>
2023-08-22 20:56:39 +0300

    Challenge 4 - Done with step 4

--- JavaScript Challenges/Challenge 4/script.js
+++ JavaScript Challenges/Challenge 4/script.js
@@ -19,13 +19,17 @@ console.log("allNumbers is : ", allNumbers)
 
 let additionalInfo = new Object();
 additionalInfo = {
+
     address: "Mar Elias street, BCHAR",
-    tel_Num: "+096170606987"
+    tel_Num: "+096170606987",
+    land_line: "+96104915975",
 }
 fullInfo = { ...personalInfo, ...additionalInfo };
 console.log("fullInfo is: ", fullInfo);
 
 // Challenge 4 - Done with step 2
+// ## Step 3: Destructuring
+// ### Destructuring Arrays
 let fiveNumbers = [10, 20, 30, 40, 50];
 let firstNum1 = fiveNumbers[0];
 let secondNum1 = fiveNumbers[1];
@@ -40,5 +44,62 @@ console.log("Welcome to Destructuring Objects!");
 let restInfo;
 ({ fname, location, nationality, ...restInfo } = personalInfo);
 console.log(fname, location, nationality, "and rest of Info is ", restInfo);
-
+// output is : Zizette CH Matn Lebanese and  { age: 60, height: 162 }
 // ## Step 4: Advanced Operations
+// ### Nested Destructuring  ({})
+
+
+let contactInfo = new Object;
+({ age, height, ...contactInfo } = personalInfo);
+console.log(age, height, "and contactInfo is  ", contactInfo);
+// ### Rest with Destructuring
+
+// already done: in step3 but with 
+
+fiveNumbers = [5, 7, 10, 60];
+
+let restOfNumbers = new Array;
+[firstNum, secondNum, ...restOfNumbers] = fiveNumbers;
+console.log(firstNum, secondNum, "rest of Numbers is an array", restOfNumbers);
+
+// ### Merging and Overwriting with Spread
+personalInfo = {
+    fname: "Zizette CH",
+    age: 60,
+    height: 162,
+    location: "Matn",
+    nationality: "Lebanese"
+};
+additionalInfo = {
+    fname: ZizetteCH,
+    nationality: "Lebanese",
+    address: "Mar Elias street, BCHAR",
+    tel_Num: "+096170606987",
+    land_line: "+96104915975",
+}
+firstif:
+for (let i; i < additionalInfo.length; i++) {
+    for (let j = 0; j < personalInfo.length; j++) {
+        if (additionalInfo[i] == personalInfo[j]) {
+            delete (additionalInfo[i]);
+            continue firstif
+        }
+
+    }
+}
+// now we don't have overlapping data 
+// we can merge spread 
+fullInfo = { ...personalInfo, ...additionalInfo };
+console.log("Our merging array fullInfo is: ", fullInfo);
+
+// Our merging array fullInfo is  {
+//   fname: 'Zizette CH',
+//   nationality: 'Lebanese',
+//   address: 'Mar Elias street, BCHAR',
+//   tel_Num: '+096170606987',
+//   land_line: '+96104915975',
+//   age: 60,
+//   height: 162,
+//   location: 'Matn'
+// } 
+// and the END
