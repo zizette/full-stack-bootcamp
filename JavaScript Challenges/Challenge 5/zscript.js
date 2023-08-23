c280c00415e5417263743cfd1d65466e518e558a
Zizette <zchamaa@gmail.com>
2023-08-23 20:57:56 +0300

    Challenge 5 - Done with step 2

--- JavaScript Challenges/Challenge 5/zscript.js
+++ JavaScript Challenges/Challenge 5/zscript.js
@@ -1,14 +1,12 @@
-// const menu = document.querySelector(".menu");
-// const headers = document.querySelector('header');
-// const footers = document.querySelector('footer');
+const menu = document.querySelector(".menu");
+const headers = document.querySelector('header');
+const footers = document.querySelector('footer');
 
-
-
-// const myElement = document.querySelectorAll(body);
-// alert("length of myElement", myElement.length);
-// for (const child of myElement){
-//   console.log(child.tagName);
-// }
+const myElement = document.querySelectorAll(body);
+alert("length of myElement", myElement.length);
+for (const child of myElement){
+  console.log(child.tagName);
+}
 const collection = document.body.children;
 
 let text = "";
@@ -31,3 +29,36 @@ newDIV.style.border="1px solid black";
 newDIV.style.fontSize="24px";
 newDIV.style.fontFamily="Helvetica";
 // End "Challenge 5 - Done with step 1".
+
+const btn = document.createElement("button");
+btn.className="button";
+btn.addEventListener("mouseover",changeColor);
+function changeColor()
+{
+btn.style.backgroundColor = "red";
+}
+btn.addEventListener("mouseout",changeColorBack);
+function changeColorBack()
+{
+btn.style.backgroundColor = "white";
+}
+const container=document.getElementsById('original')
+container.addEventListener("click",logsContent);
+function logsContent() {
+ const btns= document.getElementsByTagName('button');
+ for (btn of btns)
+  { console.log(btn.innerHTML);}
+}
+// FormData extraction
+function validateForm() {
+    let x = document.forms["myForm"]["fname"].value;
+    if (x == "") {
+      alert("Name must be filled out")
+            return false;}
+
+    else {
+      return console.log("Name is ",
+      fname); 
+    }
+}
+// "Challenge 5 - Done with step 2".
