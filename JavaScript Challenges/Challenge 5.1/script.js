ccf9ed3d197897086748f54ea45f2ebb8a4e2e91
Zizette <zchamaa@gmail.com>
2023-08-24 19:25:10 +0300

    Challenge 5.1 - Done with step 3

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -32,21 +32,50 @@ bodyHead.style.borderStyle = "dotted"
 bodyD.appendChild(bodyHead)
 bodyHead.textContent = "Header "
 bodyHead.style.color = "black"
-bodyHead.addEventListener('keyclick',mover(key))
-function mover(key) {
-    switch (key) {
-        case key = "A": bodyHead.style.color = "red";
-        case key = "S": bodyHead.style.Size = "10px"
-        case "D": const bodyPara = document.createElement('p')
-            bodyHead.appendChild(bodyPara)
-            bodyPara.innerContent = "Key D was pressed!"
-        case "W": toggleV(bodyHead)
-        case " ": bodyHead.style.fontSize = "larger"
-    }
-}
-function toggleV(argElement) {
-    if (argElement.display = "block") {
-        argElement.display = "none"
-    } else { argElement.display = "block" }
-}
-// not testedyet 
+// bodyHead.addEventListener('keyclick',mover(key))
+// function mover(key) {
+//     switch (key) {
+//         case key = "A": bodyHead.style.color = "red";
+//         case key = "S": bodyHead.style.Size = "10px"
+//         case "D": const bodyPara = document.createElement('p')
+//             bodyHead.appendChild(bodyPara)
+//             bodyPara.innerContent = "Key D was pressed!"
+//         case "W": toggleV(bodyHead)
+//         case " ": bodyHead.style.fontSize = "larger"
+//     }
+// }
+// function toggleV(argElement) {
+//     if (argElement.display = "block") {
+//         argElement.display = "none"
+//     } else { argElement.display = "block" }
+// }
+// not testedyet
+// ## Step 3:
+const newform=document.createElement('form')
+document.body.appendChild(newform)
+let LabelName=document.createElement('label')
+let LabelEmail=document.createElement('label')
+let LabelPass=document.createElement('label')
+let fullNameInput=document.createElement('input') 
+let emailInput=document.createElement('input')
+let PasswordInput=document.createElement('input')
+let submitInput=document.createElement('button') 
+submitInput.innerText=" Submit "
+LabelName.innerText="Full Name:   "
+LabelPass.innerText=" Password :  "
+LabelEmail.innerText= "Email :   "
+fullNameInput.type="text"
+emailInput.type="email"
+PasswordInput.type="password"
+PasswordInput.display="none"
+newform.appendChild(LabelName)
+LabelName.innerText="Full Name:   "
+newform.append(LabelName,fullNameInput,LabelEmail,emailInput)
+newform.append(LabelPass,PasswordInput,submitInput)
+
+submitInput.addEventListener("submit",function(){
+    preventdefault()
+    console.log("No server yet to submit")
+    // console.log(this.target.value)
+})
+// "Challenge 5.1 - Done with step 3".
