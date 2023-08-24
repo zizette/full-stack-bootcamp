932f9309be48bd3a0d1a07b895ab8f2ee63287a2
Zizette <zchamaa@gmail.com>
2023-08-24 18:21:08 +0300

    Challenge 5.1 - Done with step 2

--- JavaScript Challenges/Challenge 5.1/script.js
+++ JavaScript Challenges/Challenge 5.1/script.js
@@ -8,19 +8,45 @@ function styling(argElement) {
     argElement.style.text = "#ffffff"
     argElement.style.cursor = "pointer"
     argElement.style.border = "none"
-    argElement.style.padding = "10px 20px"}
+    argElement.style.padding = "10px 20px"
+}
 
-    
+PgBtn.addEventListener('click', function () {
+    PgBtn.style.backgroundColor = "red";
+    let rnd = Math.floor(Math.random() * 1000);
+    PgBtn.innerText = `clicked, ${rnd}`
 
-    PgBtn.addEventListener('click',function(){ 
-        PgBtn.style.backgroundColor="red";
-        let rnd= Math.floor(Math.random() * 1000);
-    PgBtn.innerText=`clicked, ${rnd}`
-    
-} )
-styling(PgBtn)
-bodyD.addEventListener('click',function(){ 
-setTimeout(styling(PgBtn),6000);
-PgBtn.innerText="Time OUT"
 })
+styling(PgBtn)
+bodyD.addEventListener('click', function () {
+    setTimeout(styling(PgBtn), 6000);
+    PgBtn.innerText = "Time OUT"
+})
+// End step1
 
+const bodyHead = document.createElement('h1')
+bodyHead.style.borderWidth = "10px"
+bodyHead.style.borderColor = "green"
+bodyHead.style.borderStyle = "dotted"
+
+bodyD.appendChild(bodyHead)
+bodyHead.textContent = "Header "
+bodyHead.style.color = "black"
+bodyHead.addEventListener('keyclick',mover(key))
+function mover(key) {
+    switch (key) {
+        case key = "A": bodyHead.style.color = "red";
+        case key = "S": bodyHead.style.Size = "10px"
+        case "D": const bodyPara = document.createElement('p')
+            bodyHead.appendChild(bodyPara)
+            bodyPara.innerContent = "Key D was pressed!"
+        case "W": toggleV(bodyHead)
+        case " ": bodyHead.style.fontSize = "larger"
+    }
+}
+function toggleV(argElement) {
+    if (argElement.display = "block") {
+        argElement.display = "none"
+    } else { argElement.display = "block" }
+}
+// not testedyet 
